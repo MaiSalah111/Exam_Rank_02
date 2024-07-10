e1422460b9963d40abd5256c95d714bf4c1c6492
MaiSalah111 <158178517+MaiSalah111@users.noreply.github.com>
2024-07-10 10:13:54 +0000

    FINAL

--- Level_04/ft_itoa.c
+++ Level_04/ft_itoa.c
@@ -1,107 +1,43 @@
-// Assignment name  : ft_itoa
-// Expected files   : ft_itoa.c
-// Allowed functions: malloc
-// --------------------------------------------------------------------------------
-
-// Write a function that takes an int and converts it to a null-terminated string.
-// The function returns the result in a char array that you must allocate.
-
-// Your function must be declared as follows:
-
-// char	*ft_itoa(int nbr);
-
 #include <stdlib.h>
-
-int	countdigit(int nbr)
-{
-	int i = 0;
-	if (nbr <= 0)
-		i = 1;
-	while (nbr)
-	{
-		i++;
-		nbr = nbr / 10;
-	}
-	return (i);
-}
+#include <stdio.h>
 
 char	*ft_itoa(int nbr)
 {
-	int len = countdigit(nbr);
-	char *str = malloc(sizeof (char) * (len + 1));
-	if (!str)
-		return (NULL);
-	unsigned int number = nbr;
-	str[len] = '\0';
-	if (nbr < 0)
-	{
-		str[0] = '-';
-		number = (unsigned int)(-nbr);
-	}
-	else if (nbr == 0)
-	{
-		str[0] = '0';
-	}
-	while (number)
-	{
-		str[--len] = number % 10 + '0';
-		number = number / 10;
-	}
-	return (str);
+    int n = nbr;
+    int len = 0;
+    if (n <= 0)
+    {
+        len++;
+    }
+    while (n)
+    {
+        n /= 10;
+        len++;
+    }
+    char *result = (char *)malloc(sizeof(char) * (len + 1));
+    if(!result)
+        return(NULL);
+    result[len] = '\0';
+    if( nbr == 0)
+    {
+        result[0] = '0';
+        return(result);
+    }
+    if( nbr < 0)
+    {
+        result[0] = '-';
+        nbr = -nbr;
+    }
+    while (nbr)
+    {
+        result[--len] = nbr % 10 + '0';
+        nbr /= 10;
+    }
+    return(result);
 }
 
-#include<stdio.h>
-
-int	main(void)
-{
-	int	n;
-
-	n = 0;
-	printf("%s\n", ft_itoa(n));
-}
-
-// #include <stdlib.h>
-
-// static int	countdigt(int nbr)
+// int main (void)
 // {
-// 	int	i;
-
-// 	i = 0;
-// 	if (nbr == 0)
-// 		return (1);
-// 	while (nbr)
-// 	{
-// 		i++;
-// 		nbr = nbr / 10;
-// 	}
-// 	return (i);
-// }
-
-// char	*ft_itoa(int nbr)
-// {
-// 	unsigned int	number;
-// 	char			*str;
-// 	int				len;
-
-// 	len = countdigt(nbr);
-// 	number = nbr;
-// 	if (nbr < 0)
-// 	{
-// 		number = -1 * nbr;
-// 		len++;
-// 	}
-// 	str = malloc(sizeof(char) * (len + 1));
-// 	if (str == NULL)
-// 		return (NULL);
-// 	str[len] = '\0';
-// 	if (number == 0)
-// 		str[--len] = '0';
-// 	while (number)
-// 	{
-// 		str[--len] = number % 10 + '0';
-// 		number = number / 10;
-// 	}
-// 	if (nbr < 0)
-// 		str[0] = '-';
-// 	return (str);
+//     int nbr = -123;
+//     printf("%s", ft_itoa(nbr));
 // }
--- Level_04/sort_list.c
+++ Level_04/sort_list.c
@@ -33,26 +33,24 @@
 
 #include "list.h"
 
-void	swap(t_list *a, t_list *b)
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {
-	int tmp = a->data;
-	a->data = b->data;
-	b->data = tmp;
-	}
+    int swap;
+    t_list *tmp;
 
-	t_list *sort_list(t_list *lst, int (*cmp)(int, int))
-	{
-	t_list *tmp = lst;
-	while (lst->next)
-	{
-		if((*cmp)(lst->data, lst->next->data) == 0)
-		{
-			swap(lst, lst->next);
-			lst = tmp;
-		}
-		else 
-			lst = lst->next;
-	}
-	lst = tmp;
-	return(tmp);
+    tmp = lst;
+    while (lst->next)
+    {
+        if((*cmp) (lst->data, lst->next->data) == 0)
+        {
+            swap = lst->data;
+            lst->data = lst->next->data;
+            lst->next->data = swap;
+            lst = tmp;
+        }
+        else
+            lst = lst->next;
+    }
+    lst = tmp;
+    return (lst);
 }
