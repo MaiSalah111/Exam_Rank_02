3fda83ff43e110e96f0cf0e3154d93cc271777bd
MaiSalah111 <158178517+MaiSalah111@users.noreply.github.com>
2024-07-18 08:37:03 +0000

    FINAL

--- Level_02/wdmatch.c
+++ Level_02/wdmatch.c
@@ -40,14 +40,12 @@ void    wdmatch(char *str1, char *str2)
     }
     if (!str1[i]) 
         write(1, str1, i);
-    write(1, "\n", 1);
 }
 
 int     main(int ac, char **av)
 {
     if (ac == 3)
         wdmatch(av[1], av[2]);
-    else
-        write(1, "\n", 1);
+    write(1, "\n", 1);
     return (0);
 }
